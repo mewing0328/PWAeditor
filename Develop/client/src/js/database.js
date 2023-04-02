1672d791788ac018b2cfffb2746725cb23cc1531
MEwing <masandraewing@gmail.com>
2023-04-02 19:16:04 -0400

    utilized mini project to get the getAll method

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -36,8 +36,26 @@ export const putDb = async (content) => {
   console.log("You've successfully saved the new content.");
 };
 
+// Added logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET all notes and snippets from the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result.value;
+};
 
 initdb();
